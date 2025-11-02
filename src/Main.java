9ef68829858a9f9e8c6f46fc3a00cda3ab01a86a
snoochx <godsnooch@gmail.com>
2025-11-02 08:38:56 +0300

    Implemented majority element detection with frequency counting and detailed output

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,37 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        int[] arr = new int[10];
+        Random random = new Random();
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = random.nextInt(2);
+        }
+
+        System.out.print("Массив: ");
+        for (int num : arr) System.out.print(num + " ");
+        System.out.println();
+
+        Map<Integer, Integer> counts = new HashMap<>();
+        for (int num : arr) {
+            counts.put(num, counts.getOrDefault(num, 0) + 1);
+        }
+
+        Integer majority = null;
+        for (Map.Entry<Integer, Integer> entry : counts.entrySet()) {
+            if (entry.getValue() > arr.length / 2) {
+                majority = entry.getKey();
+                break;
+            }
+        }
+
+        if (majority != null) {
+            System.out.println("Мажоритарный элемент: " + majority);
+        } else {
+            System.out.println("Мажоритарного элемента нет");
         }
     }
 }
